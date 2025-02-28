b266e022a6ecc75760bf9f39ec0e0cbe950e5c21
Evgeniy Titov <scrbtw3am@yandex.ru>
2025-02-28 10:23:00 +0300

    My scripts Administrator

--- Очистка кэша 1с {Базы останутся}.bat
+++ Очистка кэша 1с {Базы останутся}.bat
@@ -11,5 +11,5 @@ for /d %%i in ("%USERPROFILE%\AppData\Roaming\1C\1cv8*") do rmdir /s /q "%%i"
 for /d %%i in ("%USERPROFILE%\AppData\Local\1C\*") do rmdir /s /q "%%i"
 )
 
-echo Кэш 1С успешно очищен!>nul
+echo Кэш 1С успешно очищен!!>nul
 pause
