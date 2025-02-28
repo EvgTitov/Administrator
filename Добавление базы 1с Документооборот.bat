991558607407000968e727f76d1d24c7e6b7e4b4
Evgeniy Titov <scrbtw3am@yandex.ru>
2025-02-28 09:57:38 +0300

    Add files via upload

---
+++ Добавление базы 1с Документооборот.bat
@@ -0,0 +1,21 @@
+@echo off
+for /f %%i in ('
+ find /c /v ""^<"%AppData%\1C\1CEStart\ibases.v8i"
+') do (
+ set m=%%i
+ )
+for /f "delims=:" %%i in ('
+ findstr /n $ "%AppData%\1C\1CEStart\ibases.v8i"
+') do (
+ set n=%%i
+ )
+if not %m%==%n% (
+ echo.>>"%AppData%\1C\1CEStart\ibases.v8i"
+ )
+for %%i in (
+	"[Документооборот]"
+	"Connect=Srvr="1c:1741";Ref="DOC" "
+ ) do (
+ echo %%~i>>"%AppData%\1C\1CEStart\ibases.v8i"
+ )
+exit /b
---
+++ Очистка кэша 1с {Базы останутся}.bat
@@ -0,0 +1,15 @@
+@echo off
+chcp 1251 > nul
+
+If Exist %USERPROFILE%\AppData\Roaming\1C (
+rem Удаляем все файлы в Win 7 8 10
+Del /F /Q %USERPROFILE%\AppData\Roaming\1C\1cv8*.*
+Del /F /Q %USERPROFILE%\AppData\Local\1C\*.*
+
+rem Удаляем все каталоги в Win 7 8 10
+for /d %%i in ("%USERPROFILE%\AppData\Roaming\1C\1cv8*") do rmdir /s /q "%%i"
+for /d %%i in ("%USERPROFILE%\AppData\Local\1C\*") do rmdir /s /q "%%i"
+)
+
+echo Кэш 1С успешно очищен!>nul
+pause
