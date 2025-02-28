c5d65d8566978b1f99f3e5b86520fd8832a0018d
Evgeniy Titov <scrbtw3am@yandex.ru>
2025-02-28 10:20:29 +0300

    My scripts Administrator

--- Добавление базы 1с Полимертех.bat
+++ Добавление базы 1с Полимертех.bat
@@ -13,7 +13,7 @@ if not %m%==%n% (
  echo.>>"%AppData%\1C\1CEStart\ibases.v8i"
  )
 for %%i in (
-	"[Название базы]"
+	"[Название базы 1с]"
 	"Connect=Srvr="Ваш сервер";Ref="Имя базы" "
  ) do (
  echo %%~i>>"%AppData%\1C\1CEStart\ibases.v8i"
