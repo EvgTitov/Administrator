d453513c0a45900d0a89212e5727d4bfa0389916
Evgeniy Titov <scrbtw3am@yandex.ru>
2025-02-28 10:18:08 +0300

    Update Добавление базы 1с Полимертех.bat

--- Добавление базы 1с Полимертех.bat
+++ Добавление базы 1с Полимертех.bat
@@ -13,8 +13,8 @@ if not %m%==%n% (
  echo.>>"%AppData%\1C\1CEStart\ibases.v8i"
  )
 for %%i in (
-	"[Полимертех]"
-	"Connect=Srvr="1c:1741";Ref="tasma1" "
+	"[Название базы]"
+	"Connect=Srvr="Ваш сервер";Ref="Имя базы" "
  ) do (
  echo %%~i>>"%AppData%\1C\1CEStart\ibases.v8i"
  )
