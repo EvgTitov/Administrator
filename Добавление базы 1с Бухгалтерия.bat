64c37f3e7025652ec46430ad159329ffea885dd7
Evgeniy Titov <scrbtw3am@yandex.ru>
2025-02-28 10:18:52 +0300

    Update Добавление базы 1с Бухгалтерия.bat

--- Добавление базы 1с Бухгалтерия.bat
+++ Добавление базы 1с Бухгалтерия.bat
@@ -13,8 +13,8 @@ if not %m%==%n% (
  echo.>>"%AppData%\1C\1CEStart\ibases.v8i"
  )
 for %%i in (
-	"[Бухгалтерия]"
-	"Connect=File="\\DESKTOP-C25IL9F\Bases\БП" "
+	"[Название базы]"
+	"Connect=Srvr="Ваш сервер";Ref="Имя базы" "
  ) do (
  echo %%~i>>"%AppData%\1C\1CEStart\ibases.v8i"
  )
